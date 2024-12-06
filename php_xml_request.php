8a67c6559b1d83e8b7046bbe7e4669def2240008
aron9711 <kooperpc@hotmail.com>
2024-12-06 12:08:22 +0100

    Update php_xml_request.php

--- php_xml_request.php
+++ php_xml_request.php
@@ -15,9 +15,7 @@ curl_setopt_array($curl, array(
     "type": "xml"
 }',
   CURLOPT_HTTPHEADER => array(
-    'Content-Type: application/json',
-    'Authorization: ••••••',
-    'Cookie: _tracking_consent=%7B%22con%22%3A%7B%22CMP%22%3A%7B%22a%22%3A%22%22%2C%22m%22%3A%22%22%2C%22p%22%3A%22%22%2C%22s%22%3A%22%22%7D%7D%2C%22v%22%3A%222.1%22%2C%22region%22%3A%22HUBU%22%2C%22reg%22%3A%22GDPR%22%7D'
+    'Authorization: Bearer TOKEN',
   ),
 ));
 
